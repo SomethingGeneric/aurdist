3af5789ba73a73d557f9d720b0d02c96ca8bcf2e
Matt C <matt@xhec.dev>
2025-09-01 21:53:20 -0400

    it works!

--- README.md
+++ README.md
@@ -3,8 +3,28 @@
 `aurdist` is a simple tool for building and managing your own repository of AUR packages. It uses a containerized build system to ensure that packages are built in a clean and reproducible environment.
 
 ## Features
-
 * Build AUR packages in a containerized Arch Linux environment.
 * Manage a pacman repository of your own packages.
-* Serve the repository over HTTP.
-* Push the repository to a remote server
+
+## Use
+Pacman dependencies: `sudo pacman -Sy --noconfirm base-devel pacman-contrib git docker pigz docker-buildx rsync`
+
+1. Clone repo on an Arch box w/ docker installed (ensure your user is in Docker group & has passwordless sudo)
+2. Packages get built in `packages/` under the repo. If you'd like them to be rsync'd somewhere else, e.g. where nginx is expecting them, then do: `echo "PATH" > .where` (and ensure you've installed `rsync` from repos)
+3. Run either `./build.sh <package_name>` or put a list of your favorite AUR packages into `targets.txt` and run `./build.sh` with no args
+4. Put either your web server URL or your path to `packages/` into your `/etc/pacman.conf` (if it's a web server, then you can have a dedicated package building box!)
+
+## Config Examples
+Local folder:
+```
+[aurdist]
+SigLevel = Never
+Server = file:///home/you/aurdist/packages
+```
+
+HTTP: (assuming localhost, otherwise use remote IP)
+```
+[aurdist]
+SigLevel = Never
+Server = http://127.0.0.1/
+```
--- build.sh
+++ build.sh
@@ -2,23 +2,45 @@
 
 set -e
 
-PACKAGE_NAME=$1
-
-if [ -z "$PACKAGE_NAME" ]; then
-    echo "Usage: $0 <package-name>"
-    exit 1
-fi
-
 # Create a directory to store the built packages
 mkdir -p packages
 
 # Build the Docker image
 docker build -t aur-builder .
 
-# Run the Docker container
-docker run --rm -v "$(pwd)/packages:/packages" aur-builder "$PACKAGE_NAME"
+# Function to build a single package
+build_package() {
+    local package_name=$1
+    echo "Building package: $package_name"
+    # Run the Docker container
+    docker run --rm -v "$(pwd)/packages:/packages" aur-builder "$package_name"
+}
 
+if [ -z "$1" ]; then
+    if [ -f "targets.txt" ]; then
+        echo "No package name provided, building all packages from targets.txt"
+        while IFS= read -r package; do
+            if [ -n "$package" ]; then
+                build_package "$package"
+            fi
+        done < "targets.txt"
+    else
+        echo "Usage: $0 <package-name>"
+        echo "Or create a 'targets.txt' file with a list of packages to build."
+        exit 1
+    fi
+else
+    build_package "$1"
+fi
+
+## TODO: i suppose some folks might not have their packages repo on an Arch box
 # Run a container to create/update the package repository
-docker run --rm -v "$(pwd)/packages:/packages" -w /packages aur-builder repo-add aurdist.db.tar.gz *.pkg.tar.zst
+#docker run --rm -v "$(pwd)/packages:/packages" -w /packages aur-builder repo-add aurdist.db.tar.zst *.pkg.tar.zst
 
-echo "Package built and repository updated in the 'packages' directory."
+pushd packages
+repo-add -n aurdist.db.tar.zst *.pkg.tar.zst
+popd
+
+echo "Packages built and repository updated in the 'packages' directory."
+
+[[ -f .where ]] && sudo rsync -avc packages/ $(cat .where) && echo "Rsync'd to nginx directory"
